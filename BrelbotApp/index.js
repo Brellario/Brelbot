1c99efb85530940a388b5ef251fc1376afdf7079
Brellario <143358745+Brellario@users.noreply.github.com>
2026-06-07 20:12:52 +0100

    Updated Brelbot to use env variables

--- BrelbotApp/index.js
+++ BrelbotApp/index.js
@@ -2,7 +2,8 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
-const { token } = require('./config.json');
+require('dotenv').config();
+const token = process.env.TOKEN;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
